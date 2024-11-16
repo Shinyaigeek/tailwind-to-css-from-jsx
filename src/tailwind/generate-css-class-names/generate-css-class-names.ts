99ea5dccdf9ea26b0be256f996c24122819bafb8
Shinyaigeek <Philispaxil@gmail.com>
2024-11-17 02:46:36 +0900

    Generate random css class names

---
+++ src/tailwind/generate-css-class-names/generate-css-class-names.ts
@@ -0,0 +1,9 @@
+import random from "jsr:@yuhenabc/random";
+
+export const generateCSSClassNames: (marker: string, count?: number) => string =
+  function (
+    marker,
+    count = 10,
+  ) {
+    return `${marker}-${random(count)}`;
+  };
