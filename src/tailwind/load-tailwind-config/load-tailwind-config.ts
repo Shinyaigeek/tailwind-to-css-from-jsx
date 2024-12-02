94c5d29571a4692a8e7a9a4d1f4b882948d31895
shinyaigeek <shinobu.hayashi@ubie-partners.com>
2024-12-03 00:06:17 +0900

    Read tailwindcss config file and evaluate it with dynamic import based on blob

--- src/tailwind/load-tailwind-config/load-tailwind-config.ts
+++ src/tailwind/load-tailwind-config/load-tailwind-config.ts
@@ -5,8 +5,12 @@ import { join } from "@std/path";
 export const loadTailwindConfig: (p: string) => Promise<Result<Config, Error>> =
   async function (p) {
     const absPath = join(Deno.cwd(), p);
+    
     try {
-      return createOk(await import(`file://${absPath}`));
+      const tailwindCSSConfigFileContents = await Deno.readTextFile(absPath);
+      const blob = new Blob([tailwindCSSConfigFileContents], { type: 'application/javascript' });
+      const blobUrl = URL.createObjectURL(blob);
+      return createOk(await import(blobUrl));
     } catch (e) {
       return createErr(
         new Error(`Failed to load Tailwind config from ${absPath}: ${e}`),
